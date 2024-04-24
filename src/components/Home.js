bf44eb31976590e5ac6db11933c21084707cf8ba
jwathika <wathika02@gmail.com>
2024-04-24 22:12:01 +0300

    Update About and Home components

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
 	return (
 		<div id='home'>
 			<h1 style={{ color: 'firebrick' }}>
-				Your {name} is a Web Developer from Your {city}
+				{name} is a Web Developer from {city}
 			</h1>
 		</div>
 	);
